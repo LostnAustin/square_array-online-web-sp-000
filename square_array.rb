20b318f9e86af7aea8da47b9a37711aca9933f2a
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-23 14:55:45 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,55 @@
+require "pry"
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |number|
 end
+
+end
+#new_array = []
+ #array.each do |number|
+  # new_array[number]
+#end
+#  binding.pry
+#return new_array
+#end
+
+    #array = array *= array
+ #puts "#{array}"
+ #returns [1,4,9]
+  #  puts "#{array *= array}"
+    #  returns [1,4,9]
+
+  # new_array = puts array *= array
+
+    #  returns [1,4,9]
+
+    #new_array = puts "#{(array *= array)}"
+    #new_array
+    #returns [1,4,9]
+
+
+#puts "#{new_array}"
+#returns [1,2,3]
+
+ #return array *= array
+#results in 1
+
+#  puts ("#{array}" * "#{array}")
+  #returns  TypeError:  no implicit conversion of String into Integer
+
+#puts "#{array}"
+  #returns [1,2,3]
+
+#array ** 2
+#puts "#{array}"
+#returns [1,2,3]
+
+#new_array = array ** array
+#puts "#{new_array}"
+#returns [1,2,3]
+
+#return "#{array}"
+#returns 1
+
+
+  #return array
