3cee6aff1e0639c1c8ed6c6e30b2ec9de55e915f
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-23 15:01:01 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   new_array =[]
   number = 0
   array.each do |number|
-    new_array << array[square_array] ** 2
+  #  new_array << array[square_array] ** 2
     number += 1
 end
   new_array
