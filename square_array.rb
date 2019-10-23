9db44851de1204393585aae25075faa1b8df4e63
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-23 15:50:48 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,13 @@
 require "pry"
 def square_array(array)
   new_array =
-#array.each do |num|
-array.each do {|num| num **2}
-#  num * num
-#  puts "#{new_array}"
+array.each do |num|
+ num * num
+ puts "#{new_array}"
 
-    #new_array << square_array[array] ** 2
+
 end
 end
+
+  #new_array << square_array[array] ** 2
+#array.each do {|num| num **2}
